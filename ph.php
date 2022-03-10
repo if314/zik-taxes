83e79f482b3b6d731e74f4928c5c161406f6f895
Iva Keranova <keranovai@gmail.com>
2022-03-10 12:46:07 -0500

    test

--- ph.php
+++ ph.php
@@ -2,3 +2,4 @@
 setcookie('cookie1', 'value1', ['samesite' => 'Lax']);
 // Set a cross-site cookie for third-party contexts
 setcookie('cookie2', 'value2', ['samesite' => 'None', 'secure' => true]);
+SameSite=Lax
